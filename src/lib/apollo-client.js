43c1a2f3b9266f5753133a9b3bbcb0fbee9edfdf
colbyfayock <fayock@gmail.com>
2022-07-06 14:15:02 -0400

    Step 0: disabling Apollo Client cache for tutorial

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -23,5 +23,15 @@ export function _createApolloClient() {
       uri: process.env.WORDPRESS_GRAPHQL_ENDPOINT,
     }),
     cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore',
+      },
+      query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+      },
+    }
   });
 }
